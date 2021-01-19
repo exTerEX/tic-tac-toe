7f489ad024422b9d0dca2cf86b97c406c7c3fd6e
exTerEX <developer@sagen.io>
2021-01-19 01:10:29 +0100

    Removed general misc

--- utils/write_resource_source.py
+++
@@ -1,8 +0,0 @@
-import json
-
-current_resource_map = [
-    "resources", "resources/audio", "resources/font", "resources/icon"
-]
-
-with open("tic-tac-toe/resources/source.json", "w") as file:
-    json.dump(current_resource_map, file, indent=2)
